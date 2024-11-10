c2d5130d9a89c0ae3d4fdc61979086e6d9dcf4fc
Krish Kittur <kittur.krish@gmail.com>
2024-11-10 00:41:31 -0500

    Real initial commit

---
+++ drivebrain_core_base/include/public/DrivebrainBase.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <Configurable.hpp>
+#include <Controller.hpp>
+#include <Driver.hpp>
+#include <PubSub.hpp>
+#include <Telemetry.hpp>
+#include <VehicleManager.hpp>
---
+++ drivebrain_core_base/include/public/Driver.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace comms
+{
+/// @brief This class is what all drivers implement / look like. 
+//         They will all have a parameter update function that has to be registered, 
+//         a function for initialization, handlers for rx and tx (will be ran in threads)
+
+
+class Driver 
+{
+    public: 
+        Driver() = default;
+        ~Driver() = default;
+
+        virtual bool init() = 0;
+    private:
+         
+
+};
+}
